bc45b078488e34e4a9262f0b01c960fc26cd7c7b
Zaid alshibi <zaidealshibi@gmail.com>
2022-08-03 02:47:41 +0300

    adding weather and movie APIs

--- server.js
+++ server.js
@@ -1,34 +1,51 @@
 'use strict'
-const data = require('./data/weather.json')
+const axios = require('axios')
 const cors = require("cors");
 const express = require('express');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3001
 
-app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-    }
+}
 )
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World')
 }
 )
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async (req, res) => {
     try {
-    let lat = req.query.lat
-    let lon = req.query.lon
-    let searchQuery = req.query.searchQuery
-    let arr = findData(lat, lon, searchQuery) 
-    let newArr= arr?.map(element => {
-        return new Forecast (`Low of ${element.low_temp}, high of ${element.high_temp} with ${element.weather.description}` ,element.datetime)
-    });
-    res.send(newArr)
+        let lat = req.query.lat;
+        let lon = req.query.lon;
+        let url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`
+        let response = await axios.get(url)
+        let data = new Weather(response.data.data[0].datetime,response.data.data[0].weather.description)
+        res.send(data)
+        }
+    catch (error) {
+        res.status(400).send(error)
     }
+}
+)
+
+class Weather {
+    constructor(datetime, description) {
+        this.date = datetime;
+        this.description = description;
+    }
+}
+
+app.get('/movies', async (req, res) => {
+    try {
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${req.query.title}`
+        let response = await axios.get(url)
+        let data = response.data.results.map(movie => new Movie(movie.title, movie.overview, movie.average_votes, movie.total_votes, movie.image_url, movie.popularity, movie.release_date))
+        res.send(data)
+        }
     catch (error) {
         res.status(400).send(error)
     }
@@ -36,20 +53,14 @@ app.get('/weather', (req, res) => {
 )
 
 
-
-
-// Functions and Classes
-function findData (lat, lon, searchQuery) {
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].lat == lat && data[i].lon == lon && data[i].city_name == searchQuery) {
-            return data[i].data
-        }
-        }
+class Movie {
+    constructor(title, overview, average_votes, total_votes, image_url, popularity, release_date) {
+        this.title = title;
+        this.overview = overview;
+        this.average_votes = average_votes;
+        this.total_votes = total_votes;
+        this.image_url = image_url;
+        this.popularity = popularity;
+        this.release_date = release_date;
     }
-
-    class Forecast {
-        constructor(description, date) {
-            this.description = description
-            this.date = date
-        }
-    }
+}
