ac6ff167017a82d2d650be6d5d7a5e80d2ce6069
Zaid alshibi <zaidealshibi@gmail.com>
2022-08-04 12:24:12 +0300

    solving lab 10

--- modules/weather.js
+++ modules/weather.js
@@ -1,18 +1,25 @@
 const axios = require('axios');
 
+const cache = {};
 
 async function weather(req, res){
-    try {
+        const returnedData = [];
         let lat = req.query.lat;
         let lon = req.query.lon;
-        let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`
-        let response = await axios.get(url)
-        let data = response.data.data.map(item => new Weather(item.datetime,item.weather.description))
-        res.send(data)
+        let searchQuery = req.query.searchQuery;
+        let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${process.env.WEATHER_API_KEY}`;
+        if (cache[searchQuery] === undefined) {
+        await axios.get(url).then( response => {response.data.data.forEach(item => { let obj =  new Weather(item.datetime,`Low of ${item.low_temp}, high of ${item.max_temp} with ${item.weather.description}`);
+            returnedData.push(obj)})}
+        ).catch(error => {
+            res.status(400).send(error)
         }
-    catch (error) {
-        res.status(400).send(error)
-    }
+        )
+        cache[searchQuery] = returnedData;}
+        else {
+            returnedData.push(cache[searchQuery])
+        }
+        res.send(returnedData)
 }
 
 module.exports = weather;
